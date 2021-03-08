a9a8a100c3f32599dc8da81e6d83d1f5ca8b415c
nacong <nabinyang37@gmail.com>
2021-03-08 16:06:59 +0900

    first

---
+++ test.php
@@ -0,0 +1,14 @@
+<!document html>
+<html>
+<body>
+    <h1>Number & String</h1>
+    <?php
+    echo "hello I'm nacong";
+    ?>
+    <h1>Variable</h1>
+    <?php
+    $a = 10;
+    echo "hihi My Variable number is ".$a;
+    ?>
+</body>
+</html>
