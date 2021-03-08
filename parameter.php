f12e796fbf57c1fd1a3db80f145bca357a7e40cc
nacong <nabinyang37@gmail.com>
2021-03-08 16:30:24 +0900

    first

---
+++ parameter.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+    </head>
+    <body>
+    <?php
+    echo "안녕, ".$_GET['name']." ".$_GET['address']."에 사는구나!";
+    ?>
+    </body>
+</html>
